3b6bd5c575efab24537f7971dfe4c779a77cb824
Christian Le <christianle@berkeley.edu>
2014-08-27 19:03:10 -0700

    doesnt work for github apache server

--- js/appRouting.js
+++ js/appRouting.js
@@ -1,6 +1,4 @@
 cmyk.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
-  $locationProvider.html5Mode(true);
-
   $routeProvider
     .when('/', {
       templateUrl: 'views/home.html',
