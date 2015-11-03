0413fd41ea5948381f8ab3d2d13f82e52029c9f1
Christian Le <christianle94@gmail.com>
2015-11-02 20:29:21 -0800

    remove angular stuff

--- js/controllers/MainCtrl.js
+++
@@ -1,9 +0,0 @@
-cmyk.controller('MainController', function($scope, $location) {
-  $scope.place = function(url, disabled) {
-    if (!disabled) {
-      $location.path("/" + url);
-    }
-  }
-
-  $scope.pageClass = 'page-home';
-});
